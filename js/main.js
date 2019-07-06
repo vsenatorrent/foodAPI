5532a3647c0d34d83359b94061371eca97c71bf4
vsenatorrent <urpride1@gmail.com>
2019-07-06 17:57:00 +0300

    bit styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const getCardContaier = () => {
-    const card = $('<div>', {class: 'card'});
-    const cardImage = $('<img>', {class: 'card-img-top'});
-    const cardBody = $('<div>', {class: 'card-body'});
-    const cardTitle = $('<h5>', {class: 'card-title'});
+const getCardContaier = (imgPath, titleContent) => {
+    const getElem = (selector, classname) => {
+        const elem = $(`<${selector}>`, {class: classname })
+        return elem;
+    }
+    // const card = $('<div>', {class: 'card'});
+    const card = getElem('div', 'card my-2');
+    // const cardImage = $('<img>', {class: 'card-img-top'});
+    const cardImage = getElem('img', 'card-img-top').attr('src', imgPath);
+    // const cardBody = $('<div>', {class: 'card-body'});
+    const cardBody = getElem('div', 'card-body');
+    // const cardTitle = $('<h5>', {class: 'card-title'});
+    const cardTitle = getElem('h5', 'card-title').text
+    ('label: ' + titleContent);
     cardBody.append(cardTitle);
     // $('body').append(card, [cardImage, cardBody]);
     card.append(cardImage);
@@ -12,6 +21,7 @@ const getCardContaier = () => {
 
 $('#index-form').on('submit', function(e){
     e.preventDefault();
+    $('.card-deck').html('');
     const id = 'a695cc1d';
     const key = '26f6e53414787742b18d4ffe0a73982e';
     const inputData = $('.index-form__input').val();
@@ -25,9 +35,9 @@ $('#index-form').on('submit', function(e){
             const path = val.food;
             // console.log(path);
             if(path.image && path.label){
-                const card = getCardContaier();
+                const card = getCardContaier(path.image, path.label);
                 //insert card data
-                $('.card-group').append(card);
+                $('.card-deck').append(card);
             }
         })
     })
