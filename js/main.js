a444a1c6ee0c2af300fb5a6a404fdeb82cd01221
vsenatorrent <urpride1@gmail.com>
2019-07-09 20:45:35 +0300

    new food api added

--- js/main.js
+++ js/main.js
@@ -1,45 +1,73 @@
-const getCardContaier = (imgPath, titleContent) => {
-    const getElem = (selector, classname) => {
-        return $(`<${selector}>`, {class: classname })
-    };
-    // const card = $('<div>', {class: 'card'});
-    const card = getElem('div', 'card my-2');
-    // const cardImage = $('<img>', {class: 'card-img-top'});
-    const cardItem = getElem('div', 'card-item');
-    const cardImage = getElem('img', 'card-img-top').attr('src', imgPath);
-    // const cardBody = $('<div>', {class: 'card-body'});
-    const cardBody = getElem('div', 'card-body');
-    // const cardTitle = $('<h5>', {class: 'card-title'});
-    const cardTitle = getElem('h5', 'card-title').text
-    (titleContent);
-    cardBody.append(cardTitle);
-    // $('body').append(card, [cardImage, cardBody]);
-    card.append(cardImage);
-    card.append(cardBody);
-    cardItem.append(card);
-    return cardItem;
+
+
+// $('#index-form').on('submit', function(e){
+//     e.preventDefault();
+//     $('.card-deck').html('');
+//     const id = 'a695cc1d';
+//     const key = '26f6e53414787742b18d4ffe0a73982e';
+//     const inputData = $('.index-form__input').val();
+//     fetch(`https://api.edamam.com/api/food-database/parser?ingr=${inputData}&app_id=${id}&app_key=${key}`)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(response){
+//         const responseArray = response.hints;
+//         $.each(responseArray, function(index,val){
+//             const path = val.food;
+//             // console.log(path);
+//             if(path.image && path.label){
+//                 const card = getCardContaier(path.image, path.label);
+//                 //insert card data
+//                 $('.card-deck').append(card);
+//             }
+//         })
+//     })
+// });
+
+
+const createCard = (meal) => {
+    const card = $('<div>', { class:'card-wrapper' });
+    const content = `
+        <div class="cards__item">
+            <img src="${meal.mealThumb}" alt=""
+                class="cards__image">
+            <div class="cards__description">
+                <span class="cards__name">${meal.mealName} (${meal.mealArea})</span>
+                <div class="cards__profit">
+                    <span class="cards__calories">255 calories</span>
+                    <span class="cards__ingredients">16 ingredients</span>
+                </div>
+                <a href="${meal.mealLink}" class="cards__link"></a>
+                <span class="cards__watch">watch cooking</span>
+            </div>
+        </div>`
+    card.html(content);
+    return card;
 }
 
-$('#index-form').on('submit', function(e){
+$('.index-form').on('submit', function(e){
     e.preventDefault();
-    $('.card-deck').html('');
-    const id = 'a695cc1d';
-    const key = '26f6e53414787742b18d4ffe0a73982e';
+    $('.cards').html('');
     const inputData = $('.index-form__input').val();
-    fetch(`https://api.edamam.com/api/food-database/parser?ingr=${inputData}&app_id=${id}&app_key=${key}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputData}`)
     .then(function(response){
         return response.json();
     })
     .then(function(response){
-        const responseArray = response.hints;
-        $.each(responseArray, function(index,val){
-            const path = val.food;
-            // console.log(path);
-            if(path.image && path.label){
-                const card = getCardContaier(path.image, path.label);
-                //insert card data
-                $('.card-deck').append(card);
-            }
-        })
+       console.log(response.meals);
+       const responseObj = response.meals;
+       $.each(responseObj, function(index,val){
+        const meal = {
+            mealArea: val.strArea,
+            mealName: val.strCategory,
+            mealLink: val.strYoutube,
+            mealThumb: val.strMealThumb
+        }
+        const card = createCard(meal);
+        $('.cards').append(card);
+       })
+       
+      
     })
 });
+
